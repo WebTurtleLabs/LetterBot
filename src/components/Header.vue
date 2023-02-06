079524df4cc92e25869c9a11bf4bc7dbba973ee7
sovintz <lan.sovinc@gmail.com>
2023-02-06 15:02:38 +0100

    Header text position fix.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <div style="height:100vh; position:relative;">
     <v-container style="height:100%">
       <v-row style="height:100%;">
-        <v-col cols="12" align-self="center" class="v-col-md-8">
+        <v-col cols="12" :align-self="smAndDown ? 'start' : 'center'" class="v-col-md-8" :class="smAndDown && 'pt-16'">
           <div class="text-h2 text-sm-h1 text-over-svg">LetterBot</div>
           <div class="text-h6 text-sm-h5 text-over-svg">Next Gen AI-Powered Motivation Letter Generator</div>
           <v-btn class="my-3 text-over-svg" size="x-large" rounded="pill" color="primary" @click="$goTo('generate')">Begin</v-btn>
@@ -18,7 +18,7 @@
 
 <script setup>
 import { useDisplay } from 'vuetify'
-const { mdAndUp } = useDisplay();
+const { smAndDown, mdAndUp } = useDisplay();
 </script>
 
 <style scoped>

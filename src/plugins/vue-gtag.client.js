7c9d56f74ae029a3b2b09e6db0d57db2fdc27546
sovintz <lan.sovinc@gmail.com>
2023-02-10 02:47:07 +0100

    Added Google Tag Manager 2.

--- src/plugins/vue-gtag.client.js
+++ src/plugins/vue-gtag.client.js
@@ -3,7 +3,6 @@ export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(VueGtag, {
         config: {
             id: useRuntimeConfig().public.gaMeasurementId
-        },
-        bootstrap: false,
+        }
     }, nuxtApp.$router)
 })

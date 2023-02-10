fefefae1e311bef37bcbd1d255ce20c2ca13315c
sovintz <lan.sovinc@gmail.com>
2023-02-10 13:30:08 +0100

    Anonymize IP in GA.

--- src/plugins/vue-gtag.client.js
+++ src/plugins/vue-gtag.client.js
@@ -2,7 +2,10 @@ import VueGtag from 'vue-gtag'
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(VueGtag, {
         config: {
-            id: useRuntimeConfig().public.gaMeasurementId
+            id: useRuntimeConfig().public.gaMeasurementId,
+            params: {
+                anonymize_ip: true
+            }
         }
     }, nuxtApp.$router)
 })
